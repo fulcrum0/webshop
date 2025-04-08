8fa030a530c489ad360af45e2bef65c824b73458
tarikerenkurt <tarikerenkurt@gmail.com>
2025-04-08 12:11:44 +0200

    v1.0.2
    Edited: Every HTML document in design folder changed.
    Edited: Templates are changed to English.
    Edited: Name of the shop changed.

--- dev/template/head.inc.php
+++ dev/template/head.inc.php
@@ -11,13 +11,13 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-   <title>Webshop De Witte Kip</title>
+   <title>LV Bookstore</title>
 
-   <link rel="apple-touch-icon" sizes="180x180" href="img/apple-touch-icon.png">
-   <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
-   <link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png">
+   <link rel="preconnect" href="https://fonts.googleapis.com">
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&family=VT323&display=swap" rel="stylesheet">
+   <link rel="icon" href="img/book.png">
    <link rel="manifest" href="img/site.webmanifest">
-
    <link rel="stylesheet" href="css/uikit.min.css">
    <link rel="stylesheet" href="css/style.css">
 
@@ -31,9 +31,9 @@
             <div class="uk-navbar-left">
                <ul class="uk-navbar-nav">
                   <li>
-                     <a href="/">
-                        <img class="logo" src="img/logo4.png" alt="Webshop Het Witte Kippetje" title="Webshop Het Witte Kippetje" />
-                        Het Witte Kippetje
+                     <a href="index.php">
+                        <img class="logo" src="img/book.png" alt="Book" title="Book" />
+                        LV Bookstore
                      </a>
                   </li>
                </ul>
@@ -44,36 +44,36 @@
                <ul class="uk-navbar-nav">
                   <li class="uk-active"><a href="/"><span uk-icon="icon: home"></span>Home</a></li>
                   <?php if (guest()) : ?>
-                     <li><a href="login.php"><span uk-icon="icon: sign-in"></span>Inloggen</a></li>
-                     <li><a href="register.php"><span uk-icon="icon: file-edit"></span>Registreren</a></li>
+                     <li><a href="login.php"><span uk-icon="icon: sign-in"></span>Log In</a></li>
+                     <li><a href="register.php"><span uk-icon="icon: file-edit"></span>Register</a></li>
                   <?php endif; ?>
                   <?php if (isLoggedIn()) : ?>
                      <li>
                         <a href="cart.php">
                            <span uk-icon="icon: cart"></span>
-                           Winkelwagen
+                           Cart
                            <span id="cart_amount_indicator" class="uk-badge"><?= countItemsInCart() ?></span>
                         </a>
                      </li>
                      <li>
-                        <a href="#"><span uk-icon="icon: user"></span>Welkom <?= user()->firstname ?> <span uk-navbar-parent-icon></span></a>
+                        <a href="#"><span uk-icon="icon: user"></span>Welcome <?= user()->firstname ?> <span uk-navbar-parent-icon></span></a>
                         <div class="uk-navbar-dropdown">
                            <ul class="uk-nav uk-navbar-dropdown-nav">
-                              <li class="uk-nav-header">Uw gegevens</li>
-                              <li><a href="profile.php"><span uk-icon="icon: settings"></span>Profiel</a></li>
-                              <li><a href="orderlist.php"><span uk-icon="icon: bag"></span>Bestellingen</a></li>
-                              <li><a href="invoicelist.php"><span uk-icon="icon: credit-card"></span>Facturen</a></li>
-                              <li><a href="returnlist.php"><span uk-icon="icon: refresh"></span>Retouren</a></li>
-                              <li><a href="favorites.php"><span uk-icon="icon: heart"></span>Wensenlijst</a></li>
+                              <li class="uk-nav-header">Your Information</li>
+                              <li><a href="profile.php"><span uk-icon="icon: settings"></span>Profile</a></li>
+                              <li><a href="orderlist.php"><span uk-icon="icon: bag"></span>Orders</a></li>
+                              <li><a href="invoicelist.php"><span uk-icon="icon: credit-card"></span>Invoices</a></li>
+                              <li><a href="returnlist.php"><span uk-icon="icon: refresh"></span>Return</a></li>
+                              <li><a href="favorites.php"><span uk-icon="icon: heart"></span>Wish List</a></li>
                               <li class="uk-nav-header">Contact</li>
-                              <li><a href="customerservice.php"><span uk-icon="icon: info"></span>Klantenservice</a></li>
+                              <li><a href="customerservice.php"><span uk-icon="icon: info"></span>Customer Service</a></li>
                               <li class="uk-nav-divider"></li>
                               <li>
                                  <form method="POST" action="logout.php" id="logout-form" style="display: none;">
                                     <input type="hidden" name="user_id" value="<?= user_id() ?>" />
                                  </form>
                                  <a href="javascript:void" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                    <span uk-icon="icon: sign-out"></span>Uitloggen
+                                    <span uk-icon="icon: sign-out"></span>Log out
                                  </a>
                               </li>
                            </ul>
